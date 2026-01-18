fe4085a004e4f37748dd5931353927a29bab9649
semibluff888 <632117529@qq.com>
2026-01-18 16:41:44 +0800

    Add custom modifications and setup .gitignore for large files

---
+++ run_demo.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# 显式加载 Conda 的基础配置
+# 注意：把下面的路径改成你自己的conda路径（用 which conda 找）
+# source ~/anaconda3/etc/profile.d/conda.sh
+# 如果是 miniconda
+source ~/miniconda3/etc/profile.d/conda.sh
+
+conda activate deepseek-ocr
+
+python web_demo.py
